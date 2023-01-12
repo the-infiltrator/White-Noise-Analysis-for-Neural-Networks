4007788da9b67a09eff829661a11a73c95f87418
Nayan Saxena <photosbynayan@gmail.com>
2023-01-11 19:04:01 -0500

    Typo in imports fixed

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-mport os
+import os
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -9,7 +9,6 @@ import torchvision
 import torchvision.transforms as transforms
 from typing import Tuple, Dict, List
 
-from model import get_model 
 
 def get_data_loader(dataset, batch_size, shuffle=True, num_workers=4, pin_memory=False): 
   
